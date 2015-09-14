31b597d23f481deb9f2e2a1df3168326e534c364
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2015-09-14 11:26:06 -0500

    Create cpp-input-and-output.cpp
    https://www.hackerrank.com/challenges/cpp-input-and-output
    All Domains > C++ > Introduction > Input and Output

---
+++ challenges/cpp-input-and-output.cpp
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/cpp-input-and-output
+// All Domains > C++ > Introduction > Input and Output
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a+b+c << endl;
+
+    return 0;
+}

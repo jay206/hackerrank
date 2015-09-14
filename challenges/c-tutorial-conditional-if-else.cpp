c8f9700a7f4eb2d9b66ae2b862b380abc959328a
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2015-09-14 11:35:58 -0500

    Create c-tutorial-conditional-if-else.cpp
    https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else
    All Domains > C++ > Introduction > Conditional Statements

---
+++ challenges/c-tutorial-conditional-if-else.cpp
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else
+// All Domains > C++ > Introduction > Conditional Statements
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+  int n;
+  cin >> n;
+  switch (n) {
+    case 1: cout << "one" << endl; break;
+    case 2: cout << "two" << endl; break;
+    case 3: cout << "three" << endl; break;
+    case 4: cout << "four" << endl; break;
+    case 5: cout << "five" << endl; break;
+    case 6: cout << "six" << endl; break;
+    case 7: cout << "seven" << endl; break;
+    case 8: cout << "eight" << endl; break;
+    case 9: cout << "nine" << endl; break;
+    default: cout << "Greater than 9" << endl;
+  }
+    return 0;
+}

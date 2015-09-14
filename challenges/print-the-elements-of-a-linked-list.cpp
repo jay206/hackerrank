227b63ee680d412f4b6bebb75260124e23a7c95d
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2015-09-14 13:06:44 -0500

    Create print-the-elements-of-a-linked-list.cpp
    https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list
    All Domains > Data Structures > Linked Lists > Print the elements of a linked list

---
+++ challenges/print-the-elements-of-a-linked-list.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list
+// All Domains > Data Structures > Linked Lists > Print the elements of a linked list
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+  if (head == NULL) {
+    return;
+  }
+  cout << head->data << endl;
+  Print(head->next);
+}

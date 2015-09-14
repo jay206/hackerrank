911a34a8b8d27b2268fe3bb963605cac4a02a5c2
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2015-09-14 12:49:34 -0500

    Create solve-me-first.c
    https://www.hackerrank.com/challenges/solve-me-first
    All Domains > Algorithms > Warmup > Solve me first

---
+++ challenges/solve-me-first.c
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/solve-me-first
+// All Domains > Algorithms > Warmup > Solve me first
+#include <stdio.h>
+
+int main() {
+  int n1,n2;
+  scanf("%d %d",&n1,&n2);
+  printf("%i\n", n1 + n2);
+  return 0;
+}

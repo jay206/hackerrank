784084d380db7058ed75d15047e33cb6c9847609
Jeremiah McMullen <jeremiahmcmullen206@gmail.com>
2015-09-14 11:21:05 -0500

    Create cpp-hello-world.cpp
    https://www.hackerrank.com/challenges/cpp-hello-world
    All Domains > C++ > Introduction > Hello, World!

---
+++ challenges/cpp-hello-world.cpp
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/cpp-hello-world
+// All Domains > C++ > Introduction > Hello, World!
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
